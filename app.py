046b2f22bbb6d49e07cbfaa8daf94bf16003d78e
Bow (Juno) <33423299+StreetTT@users.noreply.github.com>
2024-09-24 16:45:35 +0100

    OAuth2 Test
    I have never understood the OAuth2 flow but I think I've got it. Testing it now

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
+from dotenv import load_dotenv as LoadEnvVariables
+from os import getenv
+import requests
+
+LoadEnvVariables()
 app = Flask(__name__)
+app.config['SECRET_KEY'] =  getenv("FLASK_KEY")
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    variables = {"url": getenv("NOTION_AUTH_URL")}
+    return make_response(render_template("index.html", **variables))
 
-if __name__ == "__main_":
+@app.route('/notioned')
+def notioned():
+    code = requests.args.get('code', default = "", type = str)
+
+    res = requests.post(
+        "https://api.notion.com/v1/oauth/token",
+        {
+            "grant_type":"authorization_code",
+            "code":code,
+            "redirect_uri":"https%3A%2F%2Fnotionpoolcs.onrender.com%2Fnotioned"
+        },
+        headers={
+            "Authorization": f"Basic {getenv('OAUTH_CLIENT_ID')}:{getenv('OAUTH_CLIENT_SECRET')}",
+            "Content-Type": "application/json"
+        }
+    )
+    with open("response.txt", "w") as f:
+        f.write(res)
+    return make_response(render_template("index.html"))
+
+if __name__ == "__main__":
     app.run(
         debug=True
     )

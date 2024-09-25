56163dce5303832c2575ce73e19f3e5386fb4c76
Bow (Juno) <33423299+StreetTT@users.noreply.github.com>
2024-09-25 10:30:26 +0100

    Encode authorization

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import os
 import requests
 from flask import Flask, render_template, make_response, request as rq
 from utils import MakeRequest
+from base64 import b64encode
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] =  os.environ.get("FLASK_KEY")
+client_auth = f"{os.environ.get('OAUTH_CLIENT_ID')}:{os.environ.get('OAUTH_CLIENT_SECRET')}"
+client_auth = b64encode(client_auth.encode()).decode()
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
     variables = {"url": os.environ.get("NOTION_AUTH_URL")}
     variables.update({"error": True})
+    variables.update({"data": {}})
     return make_response(render_template("index.html", **variables))
 
 @app.route('/notioned', methods = ["GET", "POST"])
@@ -26,7 +30,7 @@ def notioned():
             "redirect_uri":"https://notionpoolcs.onrender.com/notioned"
         },
         headers={
-            "Authorization": f"Basic {os.environ.get('OAUTH_CLIENT_ID')}:{os.environ.get('OAUTH_CLIENT_SECRET')}",
+            "Authorization": f"Basic {client_auth}",
             "Content-Type": "application/json"
         }
     )
